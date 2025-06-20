34e134581a688e9cce2f1f090d5b568b594ecdae
cammy rapson <accounts@virtualhelperuk.com>
2025-06-20 21:59:57 +0100

    test docker edit

--- scum-no-anticheat/startup.sh
+++ scum-no-anticheat/startup.sh
@@ -8,6 +8,5 @@ if [ "${UPDATE_ON_BOOT}" == "1" ]; then
 fi
 
 # Start the server
-cd /mnt/server
 echo "Starting SCUM server with Wine..."
-xvfb-run --auto-servernum wine SCUMServer.exe -log -PORT=${SERVER_PORT:-7777} -nobattleye
+xvfb-run --auto-servernum wine /mnt/server/SCUMServer.exe -log -PORT=${SERVER_PORT:-7777} -nobattleye
