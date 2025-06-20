d6e89a0770701ce067270035f81d56e6932a125e
cammy rapson <accounts@virtualhelperuk.com>
2025-06-20 21:14:55 +0100

    idk

--- scum-no-anticheat/startup.sh
+++ scum-no-anticheat/startup.sh
@@ -4,11 +4,11 @@ cd /home/container
 if [ "${UPDATE_ON_BOOT}" == "1" ]; then
     echo "Updating SCUM server via SteamCMD..."
     /steamcmd/steamcmd.sh +login anonymous \
-        +force_install_dir /home/container/scum_server \
+        +force_install_dir /mnt/server \
         +login anonymous \
         +app_update 3792580 validate +quit
 fi
 
 # Start the server
-cd /home/container/scum_server
+cd /mnt/server
 xvfb-run --auto-servernum wine SCUMServer.exe -log -PORT=${SERVER_PORT:-7777} -nobattleye
