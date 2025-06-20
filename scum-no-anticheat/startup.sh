4bd7711f85acd789ece19b37839c4505742dfed4
cammy rapson <accounts@virtualhelperuk.com>
2025-06-20 21:48:29 +0100

    change app id

--- scum-no-anticheat/startup.sh
+++ scum-no-anticheat/startup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-cd /home/container
-
 if [ "${UPDATE_ON_BOOT}" == "1" ]; then
     echo "Updating SCUM server via SteamCMD..."
-    /steamcmd/steamcmd.sh +login anonymous \
+    /mnt/server/steamcmd/steamcmd.sh +login anonymous \
         +force_install_dir /mnt/server \
         +login anonymous \
-        +app_update 3792580 validate +quit
+        +app_update 3792581 validate +quit
 fi
 
 # Start the server
 cd /mnt/server
+echo "Starting SCUM server with Wine..."
 xvfb-run --auto-servernum wine SCUMServer.exe -log -PORT=${SERVER_PORT:-7777} -nobattleye
